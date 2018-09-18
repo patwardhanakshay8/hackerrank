ae90a595f9298511677cea1b0b166177144772ec
Akshay <patwardhanakshay8@gmail.com>
2018-09-18 23:22:37 +0530

    #Day2 A Very Big Sum

---
+++ AVeryBigSum/solutions.py
@@ -0,0 +1,32 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the aVeryBigSum function below.
+def aVeryBigSum(ar):
+    sum = 0
+    for element in ar:
+        try:
+            value = int(element)
+            sum = sum  + value
+        except ValueError: 
+            pass
+    
+    return sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(raw_input())
+
+    ar = map(long, raw_input().rstrip().split())
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
